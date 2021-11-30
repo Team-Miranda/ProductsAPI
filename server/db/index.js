b7379610409d8bd391b62d13b87c2d020d7ae9d1
Amitt Dosanjh <amitt.dosanjh@gmail.com>
2021-11-30 10:52:12 -0600

    Changed connection to Postgres to use a connectionString intead of object. Added loader-io file to public

--- server/db/index.js
+++ server/db/index.js
@@ -3,14 +3,12 @@ const { USER, HOST, DATABASE, PASSWORD } = process.env;
 const { Pool, Client } = require("pg");
 
 const pool = new Pool({
-  user: USER,
-  host: HOST,
-  database: DATABASE,
-  password: PASSWORD,
+  connectionString: "postgresql://dbmanager:password@13.58.66.168:5432/SDC",
 });
 
-// pool
-//   .connect()
-//   .then(() => console.log("🐘 Connected to DB"))
-//   .catch((err) => console.error(err));
+pool
+  .connect()
+  .then(() => console.log("🐘 Connected to DB"))
+  .catch((err) => console.error(err));
+
 module.exports = pool;
